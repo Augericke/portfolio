8821940499602d017a0e8cc05e7a46274a6327c6
Augericke <augericke@gmail.com>
2022-05-23 12:50:04 -0400

    add image overaly to pcard

--- src/components/library/projectCard/index.tsx
+++ src/components/library/projectCard/index.tsx
@@ -5,43 +5,39 @@ type ProjectCardProps = {};
 
 const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps) => {
   return (
-    <div className="flex justify-center">
-      <div
-        className="flex h-[350px] w-3/5 flex-col items-start rounded-[20px]
-                      bg-primaryDark dark:bg-primary"
-      >
-        <div className="flex h-full w-3/5 justify-center ">
-          <div className="flex w-10/12 flex-col justify-evenly">
-            <div>
-              <p className="text-4xl font-bold text-white">
-                NYC Bike Crash Report
-              </p>
-              <p className=" mt-6 text-lg font-light text-white">
-                I worked on this thing for these people using these tools. You
-                can use this thing I used to do this other thing by going to
-                this page.
-              </p>
+    <div className="m-auto mt-[50px] w-3/4">
+      <div className="block h-[350px] w-3/4 rounded-[20px] bg-primaryDark dark:bg-primary">
+        <div className="ml-6 flex h-full w-7/12 flex-col justify-evenly">
+          <div>
+            <p className="text-4xl font-bold text-white">
+              NYC Bike Crash Report
+            </p>
+            <p className=" mt-6 text-lg font-light text-white">
+              I worked on this thing for these people using these tools. You can
+              use this thing I used to do this other thing by going to this
+              page.
+            </p>
+          </div>
+          <div className="mt-3 flex flex-col gap-4">
+            <div className="flex justify-end gap-8">
+              {['React', 'Typescript', 'NextJs', 'MongoDb'].map(
+                (skill, key) => {
+                  return (
+                    <p className="text-white" key={key}>
+                      {skill}
+                    </p>
+                  );
+                }
+              )}
             </div>
-            <div className="mt-3 flex flex-col gap-4">
-              <div className="flex justify-end gap-8">
-                {['React', 'Typescript', 'NextJs', 'MongoDb'].map(
-                  (skill, key) => {
-                    return (
-                      <p className="text-white" key={key}>
-                        {skill}
-                      </p>
-                    );
-                  }
-                )}
-              </div>
-              <div className="m-0 flex items-center justify-end gap-8 text-white">
-                <FaGithub size={25} />
-                <FaExternalLinkAlt size={20} />
-              </div>
+            <div className="m-0 flex items-center justify-end gap-8 text-white">
+              <FaGithub size={25} />
+              <FaExternalLinkAlt size={20} />
             </div>
           </div>
         </div>
       </div>
+      <div className="relative -top-[400px] left-2/4 block h-[350px] w-1/2 rounded-[20px] bg-pink-300 shadow-lg"></div>
     </div>
   );
 };
