8cbe8cd9aa56ef1e74f10298dc45697648be57a9
Augericke <augericke@gmail.com>
2022-05-24 21:05:47 -0400

    add small screen support

--- src/components/library/projectCard/index.tsx
+++ src/components/library/projectCard/index.tsx
@@ -6,23 +6,28 @@ type ProjectCardProps = {};
 
 const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps) => {
   return (
-    <div className="mt-12 flex justify-center">
+    <div className="mt-12 flex flex-col-reverse items-center md:flex-row md:justify-center">
       <div
-        className="overflow-hidden rounded-[20px] bg-primaryDark dark:bg-primary md:min-h-[20rem] md:w-2/3 lg:min-h-[24rem]
-                      xl:w-[850px]"
+        className="relative z-10 -mt-44 w-2/3 overflow-hidden rounded-[20px] bg-primaryDark
+                 dark:bg-primary md:z-0 md:mt-0 md:min-h-[20rem] md:w-2/3
+                   lg:min-h-[24rem] xl:w-[850px] "
       >
-        <div className="rounded-20px flex h-full w-7/12 justify-center">
+        <div className="rounded-20px flex h-full w-full justify-center md:w-7/12">
           <div className="flex w-10/12 flex-col justify-evenly">
-            <p className="lg mt-8 mb-5 font-bold text-white md:text-xl lg:text-4xl">
+            <p className="lg mt-8 mb-5 text-lg font-bold text-white md:text-xl lg:text-4xl">
               NYC Bike Crash Report{' '}
             </p>
-            <p className="text-white md:text-base lg:text-lg">
+            <p className="text-sm text-white md:text-base lg:text-lg">
               I worked on this thing for these people using these tools. You can
               use this thing I used to do this other thing by going to this
               page.
             </p>
-            <div>
-              <div className="mt-8 flex justify-start gap-8 font-light md:text-xs lg:text-base">
+            <div className="flex flex-col items-center justify-center md:w-full md:items-start">
+              <div
+                className="mt-8 flex flex-wrap justify-center gap-4
+                          gap-y-2 text-xs font-light md:justify-start
+                          md:gap-x-8 md:text-xs lg:text-base"
+              >
                 {['React', 'Typescript', 'NextJs', 'MongoDb'].map(
                   (skill, key) => {
                     return (
@@ -33,7 +38,7 @@ const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps) => {
                   }
                 )}
               </div>
-              <div className="mt-4 mb-8 flex items-center justify-start gap-4 text-white">
+              <div className="mt-4 mb-8 flex items-center justify-center gap-4 text-white md:justify-start">
                 <FaGithub size={20} />
                 <FaExternalLinkAlt size={18} />
               </div>
@@ -42,7 +47,7 @@ const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps) => {
         </div>
       </div>
       <div
-        className="relative -mt-10 overflow-hidden rounded-[20px] shadow-lg md:-ml-[25vw] md:h-80 md:w-5/12
+        className="relative h-96 w-10/12  overflow-hidden rounded-[20px] shadow-lg md:-mt-16 md:-ml-[25vw] md:h-80 md:w-5/12
                    lg:h-96 lg:w-5/12 xl:-ml-[300px] xl:h-[400px] xl:w-[500px]"
       >
         <Image
@@ -52,11 +57,13 @@ const ProjectCard: React.FC<ProjectCardProps> = (props: ProjectCardProps) => {
           placeholder="blur"
         />
         <div
-          className="absolute z-10 flex h-full w-full items-center justify-center gap-8 text-white opacity-0
-                    transition-all duration-500 ease-in-out hover:bg-primaryDark/50 hover:opacity-100 hover:dark:bg-primary/50"
+          className="absolute flex h-full w-full items-center justify-center gap-8 bg-primaryDark/70
+                   text-white dark:bg-primary/60 hover:dark:bg-primary/50 md:z-10 md:bg-transparent
+                    md:opacity-0 md:transition-all md:duration-500 md:ease-in-out
+                    md:hover:bg-primaryDark/50 md:hover:opacity-100"
         >
-          <FaGithub size={45} />
-          <FaExternalLinkAlt size={45} />
+          <FaGithub size={45} className="hidden md:grid" />
+          <FaExternalLinkAlt size={45} className="hidden md:grid" />
         </div>
       </div>
     </div>
